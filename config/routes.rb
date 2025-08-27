22cdfca6ef5a313c624dffb6c785329b3e3536c1
Jake Dahlgren <121595531+JAKEDAHLGREN@users.noreply.github.com>
2025-08-27 13:32:06 -0600

    Fixed routes and sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get  "sign_in", to: "sessions#new"
-  post "sign_in", to: "sessions#create"
-  get  "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrations#create"
+  get  "sign_in",  to: "sessions#new"
+  post "sign_in",  to: "sessions#create"
+  get  "sign_up",  to: "registrations#new"
+  post "sign_up",  to: "registrations#create"
   resources :sessions, only: [ :index, :show, :destroy ]
   resource  :password, only: [ :edit, :update ]
   namespace :identity do
